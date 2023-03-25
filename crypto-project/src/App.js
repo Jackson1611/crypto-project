a01a7f0f37f35a9e4fdc390f00bb3a9160510a4f
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-03-25 11:17:12 +0200

    Coin page is made and styled

--- crypto-project/src/App.js
+++ crypto-project/src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Account from "./routes/Account";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=15&page=1&sparkline=true";
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      //console.log(response.data);
+    });
+  }, [url]);
+
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="/" element={<Signin />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Account />} />
+      </Routes>
     </ThemeProvider>
   );
 }
--- crypto-project/src/components/Navbar.jsx
+++ crypto-project/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="rounded-div flex items-center justify-between h-20 font-bold">
       <Link to="/">
-        <h1 className="text-2xl">Cryptobase</h1>
+        <h1 className="text-2xl ml-5">Coincard</h1>
       </Link>
       <div className="hidden md:block">
         <ThemeToggle />
